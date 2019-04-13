481d8405230fc5135d212b54c3cec2111c0f8654
evel.yi <evel.yi@yff.com>
2019-04-13 10:02:13 +0800

    添加监听路由

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,12 +9,25 @@
 </template>
 
 <script>
+ /* eslint-disable */ 
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 export default {
   name: 'home',
   components: {
     HelloWorld
+  },
+  methods: {
+    routeChange (to, from) {
+      console.log("routeChange")
+      console.log("to", to)
+    }
+  },
+  watch: {
+    '$route':  { // 在组件中进行监听的时候，route需要是有子组件才行，否则要加上immediate
+      handler: 'routeChange',
+      immediate: true
+    }
   }
 }
 </script>
