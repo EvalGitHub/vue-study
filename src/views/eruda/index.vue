1810eb0804b7346d18432d4439cb37eccb0385c0
evel <957010831@qq.com>
2021-06-09 09:59:41 +0800

    feat: 添加eruda

---
+++ src/views/eruda/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+     <div id="erudaContainer"/>
+    </div>
+</template>
+<script>
+    import eruda from 'eruda';
+    export default {
+        mounted() {
+            var script = document.createElement('script'); 
+            script.src = '//cdn.jsdelivr.net/npm/eruda';
+            document.body.appendChild(script); 
+            let el = document.getElementById('erudaContainer');
+            script.onload = function () { eruda.init({
+                container: el,
+            }) }
+        }
+    }
+</script>
+<style scoped>
+    #erudaContainer {
+        height: 400px;
+        width: 400px;
+    }
+</style>
